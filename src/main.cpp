58baa0949650d318a22d966caa4facb651d25349
Timothy Takita <takitatd@gmail.com>
2022-06-14 15:04:14 +1000

    added toolchains for windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+#include "include/GLFW/glfw3.h"
+#include <GL/gl.h>
 #include <iostream>
 
-int main() { return 0; }
+int main() {
+  if (!glfwInit()) {
+    printf("ERROR: GLFW failed to intialize\n");
+    return -1;
+  };
+
+  GLFWwindow *window;
+  // Make window float in i3
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  window = glfwCreateWindow(1280, 720, "Cross test", nullptr, nullptr);
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+  return 0;
+}
